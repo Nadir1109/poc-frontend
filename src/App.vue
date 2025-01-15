ce3ca021dfc9c77834c74b8fae7db68f8d8d98f9
Nadir1109 <i522995@FontysICT.nl>
2025-01-15 18:11:52 +0100

    added text hello world

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from './components/JobList.vue'
+</script>
+
+<template>
+<h1>Hello world</h1>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/JobList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Available Jobs</h1>
+    <ul>
+      <li v-for="job in jobs" :key="job.id">{{ job.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import api from "../service/api";
+
+export default {
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+  async created() {
+    const response = await api.get("/jobs");
+    this.jobs = response.data;
+  },
+};
+</script>
